81d473cf7e34b7fdff8ac6afe91cddd623b1b7a4
chikaho <147366528+chikaho@users.noreply.github.com>
2023-12-07 10:00:47 +0900

    bmi.jsを追加

---
+++ bmi.js
@@ -0,0 +1,14 @@
+// 変数の宣言
+let userWeight;
+let userHeight;
+
+//値の代入
+userWeight = '体重';
+userHeight = '身長';
+
+// 変数の宣言・値の代入
+let userWeightnum = 68;
+let userHeightnum = 1.7;
+
+// コンソールへの出力
+console.log(userWeightnum / (userHeightnum * userHeightnum));
